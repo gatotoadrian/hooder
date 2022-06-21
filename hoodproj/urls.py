b80ba303d07b03b07e97b3c993e2d189f33fbae6
gatotoadrian <gatotoadrian@gmail.com>
2022-06-21 14:16:28 +0300

    create paths in urls .py

--- hoodproj/urls.py
+++ hoodproj/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     
     path('',include('hoodapp.urls')),
     path('accounts/register/', RegistrationView.as_view(success_url='/'),
+         
     name='django_registration_register'),
     path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
