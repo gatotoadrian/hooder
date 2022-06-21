f93aba4149d7bab6cca40703a9cc188e119dcdc6
gatotoadrian <gatotoadrian@gmail.com>
2022-06-21 14:14:34 +0300

    linked urls files

--- hoodproj/urls.py
+++ hoodproj/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
+
 from django.urls import path,include
 from django_registration.backends.one_step.views import RegistrationView
 from django.contrib.auth import views 
